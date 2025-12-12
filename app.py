bd011e4b369ec3591a1638cd533d489603ef25eb
pimpnamek-svg <pimpnamek@gmail.com>
2025-12-12 15:39:20 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
+import yfinance as yf 
+import pandas as pd
+import requests
 
 app = Flask(__name__)
 
+def run_scan():
+    return "scan complete."
 @app.route("/")
 def home():
-    return "✅ Trade Evaluator is LIVE"
+    result = run_scan()
+    return f"✅ Trade Evaluator is LIVE.<br>{result}"
 
 if __name__ == "__main__":
+    
     app.run(host="0.0.0.0", port=8080)
